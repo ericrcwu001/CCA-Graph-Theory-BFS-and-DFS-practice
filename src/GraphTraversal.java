de0c6732c8b2ff03bf729943bc23494863b40247
Adam <adam.ewing.nz@gmail.com>
2023-03-13 12:38:16 +0800

    Included comments to guide implementation.

--- src/GraphTraversal.java
+++ src/GraphTraversal.java
@@ -1,14 +1,59 @@
-import java.util.LinkedList;
-import java.util.Queue;
+public class GraphTraversal {
 
-public class GraphTraversalPractice {
+    public static int[] bfs(int[][] graph, int start) {
+        // Create a new queue.
 
-    public static int[] bfs(int[][] graph) {
+        // Create an array that will record whether we have visited each node.
 
+        // Add the starting node to the queue.
+
+        // While our queue is not empty ...
+
+        // Poll the first node from the queue.
+
+        // Mark this node as visited.
+
+        // Print the node - not fundamental to our algorithm - just so we can see the path that our algorithm takes.
+
+        // Get the neighbours of this node.
+
+        // For each neighbouring node ...
+
+        // Obtain a reference to the neighbour node.
+
+        // If the neighbour node has not yet been visited ...
+
+        // Mark the neighbour node as visited
+
+        // Add the neighbour node to the queue
+
+        return null;
     }
 
-    public static int[] dfs(int[][] graph) {
+    public static int[] dfs(int[][] graph, int start) {
+        // Create a new stack.
 
+        // Create an array that will record whether we have visited each node.
+
+        // Push the starting node onto the stack.
+
+        // While our stack is not empty ...
+
+        // Pop the top node from the stack.
+
+        // Mark this node as visited.
+
+        // Print the node - not fundamental to our algorithm - just so we can see the path that our algorithm takes.
+
+        // Get the neighbours of this node.
+
+        // For each neighbouring node ...
+
+        // Obtain a reference to the neighbour node.
+
+        // If it has not yet been visited, mark it as visited and push it onto the stack.
+
+        return null;
     }
 
 }
--- src/GraphTraversalPracticeTest.java
+++ src/GraphTraversalPracticeTest.java
@@ -1,4 +1,45 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class GraphTraversalPracticeTest {
-  
+
+    private int[][] graph;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        graph = new int[][]{
+                {1,5,7},
+                {0,5},
+                {3,6},
+                {2},
+                {6},
+                {0,1},
+                {2,4,7},
+                {0,6}
+        };
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+        graph = null;
+    }
+
+    @org.junit.jupiter.api.Test
+    void bfs() {
+        int[] expectedPath = {0,1,5,7,6,2,4,3};
+        int[] observedPath = GraphTraversal.bfs(graph, 0);
+        int index = 0;
+        for (int node: observedPath) {
+            assertTrue(node == expectedPath[index++]);
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void dfs() {
+        int[] expectedPath = {0,1,5,7,6,2,3,4};
+        int[] observedPath = GraphTraversal.dfs(graph, 0);
+        int index = 0;
+        for (int node: observedPath) {
+            assertTrue(node == expectedPath[index++]);
+        }
+    }
 }
