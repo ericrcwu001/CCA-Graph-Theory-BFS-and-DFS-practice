1fbf60be61faddeb2300aa21b1b8a09604756651
Adam <adam.ewing.nz@gmail.com>
2023-03-13 12:37:33 +0800

    Initial commit

---
+++ src/GraphTraversal.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class GraphTraversalPractice {
+
+    public static int[] bfs(int[][] graph) {
+
+    }
+
+    public static int[] dfs(int[][] graph) {
+
+    }
+
+}
